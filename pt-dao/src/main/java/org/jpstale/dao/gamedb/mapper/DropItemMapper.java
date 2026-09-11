182ef80a9721e8a5d9b8596bafc4063224fcea9b
闫茂源 <115050813@qq.com>
2026-09-11 23:00:52 +0800

    feat(dao): DropItemMapper.selectAllByDropIdGt0 + 修正 dropid 列名

--- pt-dao/src/main/java/org/jpstale/dao/gamedb/mapper/DropItemMapper.java
+++ pt-dao/src/main/java/org/jpstale/dao/gamedb/mapper/DropItemMapper.java
@@ -3,14 +3,10 @@ package org.jpstale.dao.gamedb.mapper;
 import org.jpstale.dao.gamedb.entity.DropItem;
 import com.baomidou.mybatisplus.core.mapper.BaseMapper;
 
-/**
- * <p>
- *  Mapper 接口
- * </p>
- *
- * @author pt-dao
- * @since 2026-03-15
- */
+import java.util.List;
+
 public interface DropItemMapper extends BaseMapper<DropItem> {
 
+    /** 全部 dropid>0 的掉落行（gamedb.dropitem），按 dropid/chance 排序（XML 实现）。 */
+    List<DropItem> selectAllByDropIdGt0();
 }
